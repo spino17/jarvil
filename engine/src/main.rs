be4976057712f160c4acf09edba27d286c606f5d
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-05-29 11:58:16 +0530

    adding some intial code

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+mod errors;
+
+use std::fs;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string("/Users/bhavyabhatt/Desktop/file.jv")?;
+    Ok(())
 }
