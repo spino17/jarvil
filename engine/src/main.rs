e09752103b93c0e623d999c50c193f37afc7f0ce
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-05-29 21:35:58 +0530

    adding symbol table base code

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,8 +1,10 @@
 mod errors;
 mod lexer;
+mod symbol_table;
 
 use std::fs;
 use errors::CompilationError;
+use crate::symbol_table::Scope;
 
 fn main() -> Result<(), CompilationError> {
     let contents = fs::read_to_string("/Users/bhavyabhatt/Desktop/main.jv")?;
--- engine/src/symbol_table.rs
+++ engine/src/symbol_table.rs
@@ -7,3 +7,49 @@
 // 3. During parsing phase, we add entries in the table for identifiers when we find declartion statements in the current scope.
 // 4. During parsing phase, when we encounter any statement involving identifiers then we access it for checking declartions, 
 // datatypes etc.
+
+use std::{collections::HashMap};
+
+#[derive(Debug)]
+pub struct SymbolData {
+    data_type: String,
+}
+
+#[derive(Debug)]
+pub struct Scope<'a> {
+    symbol_table: HashMap<String, SymbolData>,
+    parent_scope_ref: Option<&'a Scope<'a>>,
+}
+
+impl<'a> Scope<'a> {
+    pub fn new() -> Self {
+        Scope {
+            symbol_table: HashMap::new(),
+            parent_scope_ref: None,
+        }
+    }
+
+    pub fn new_with_parent_scope(parent_scope_ref: &'a Scope<'a>) -> Self {
+        Scope {
+            symbol_table: HashMap::new(),
+            parent_scope_ref: Some(parent_scope_ref),
+        }
+    }
+
+    pub fn set(&mut self, name: String, data_type: String) {
+        self.symbol_table.insert(name, SymbolData { data_type, });
+    }
+
+    pub fn get(&self, name: &str) -> Option<&SymbolData> {
+        match self.symbol_table.get(name) {
+            Some(value) => Some(value),
+            None => {
+                if let Some(parent_scope_ref) = self.parent_scope_ref {
+                    parent_scope_ref.get(name)
+                } else {
+                    None
+                }
+            }
+        }
+    }
+}
