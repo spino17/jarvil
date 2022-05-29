6ccaac68f18f731c3b5b513e87e961f5be773566
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-05-29 11:42:33 +0530

    deleting target

--- engine/target/debug/deps/engine.d
+++
@@ -1,5 +0,0 @@
-/Users/bhavyabhatt/Desktop/Projects/Rust-Memory-Management-101/engine/target/debug/deps/engine: src/main.rs
-
-/Users/bhavyabhatt/Desktop/Projects/Rust-Memory-Management-101/engine/target/debug/deps/engine.d: src/main.rs
-
-src/main.rs:
--- engine/target/debug/engine.d
+++
@@ -1,1 +0,0 @@
-/Users/bhavyabhatt/Desktop/Projects/Rust-Memory-Management-101/engine/target/debug/engine: /Users/bhavyabhatt/Desktop/Projects/Rust-Memory-Management-101/engine/src/main.rs
