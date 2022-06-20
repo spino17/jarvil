7a4d447a3f5d76698b937a6039bdb779c9f1a424
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-06-20 16:35:27 +0530

    small cleaning up

--- engine/src/parser/packrat.rs
+++ engine/src/parser/packrat.rs
@@ -58,7 +58,6 @@ impl PackratParser {
     }
 
     pub fn get_index(&self) -> usize {
-        println!("{:?}", self.token_vec[self.lookahead]);
         (self.token_vec[self.lookahead].start_index + self.token_vec[self.lookahead].end_index) / 2 as usize
     }
 
