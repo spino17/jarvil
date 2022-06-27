080f0303885b7fbaa0156bf4db676bdfdd20f82e
Bhavya Bhatt <bhavyabhatt17@gmail.com>
2022-06-27 08:54:46 +0530

    removing newline char from code lines

--- engine/src/lexer/helper.rs
+++ engine/src/lexer/helper.rs
@@ -83,7 +83,7 @@ pub fn extract_slash_prefix_lexeme(begin_lexeme: &mut usize,
     while forward_lexeme < code.len() {
         let next_char = code[forward_lexeme];
         if next_char == '\n' {
-            code_lines.push((Rc::new(code[*line_start_index..forward_lexeme+1].iter().collect()), *line_start_index));
+            code_lines.push((Rc::new(code[*line_start_index..forward_lexeme].iter().collect()), *line_start_index));
             *begin_lexeme = *begin_lexeme + 1;
             *line_number = *line_number + 1;
             *line_start_index = forward_lexeme + 1;
@@ -161,7 +161,7 @@ pub fn extract_hash_prefix_lexeme(begin_lexeme: &mut usize,
     while forward_lexeme < code.len() {
         let next_char = code[forward_lexeme];
         if next_char == '\n' {
-            code_lines.push((Rc::new(code[*line_start_index..forward_lexeme+1].iter().collect()), *line_start_index));
+            code_lines.push((Rc::new(code[*line_start_index..forward_lexeme].iter().collect()), *line_start_index));
             *begin_lexeme = *begin_lexeme + 1;
             *line_number = *line_number + 1;
             *line_start_index = forward_lexeme + 1;
