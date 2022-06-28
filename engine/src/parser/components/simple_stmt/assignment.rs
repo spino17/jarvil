74121b34ee0694c03e4531288891d0964165b392
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-06-28 09:20:50 +0530

    added assignment parsing

--- engine/src/parser/components/simple_stmt/assignment.rs
+++ engine/src/parser/components/simple_stmt/assignment.rs
@@ -1,8 +1,9 @@
 use crate::lexer::token;
 use crate::parser::packrat::{PackratParser, ParseSuccess};
-use crate::errors::{ParseError};
+use crate::errors::{ParseError, SemanticError};
 
 pub fn assign(parser: &mut PackratParser) -> Result<ParseSuccess, ParseError> {
+    /*
     let (_, _, token_value, data_type, _) = parser.expect_id()?;
     let (_, line_number) = parser.expect("=")?;
     let rule_index;
@@ -23,4 +24,34 @@ pub fn assign(parser: &mut PackratParser) -> Result<ParseSuccess, ParseError> {
         lookahead: parser.get_lookahead(),
         possible_err: None,
     })
+     */
+    let index = parser.get_index();
+    let (_, data_type, is_assignable) = parser.atom()?;
+    parser.expect("=")?;
+    if !is_assignable {
+        let line_number = parser.get_curr_line_number();
+        return Err(ParseError::SEMANTIC_ERROR(SemanticError::new(
+            parser.get_code_line(line_number, index),
+            String::from("value is not assignable")))
+        )
+    }
+    if let Some(l_data_type) = data_type {
+        let (response, (r_data_type, index)) = parser.param()?;
+        if !l_data_type.eq(&r_data_type) {
+            let line_number = parser.get_curr_line_number();
+            return Err(ParseError::SEMANTIC_ERROR(SemanticError::new(
+                parser.get_code_line(line_number, index),
+                format!("mismatched types\n    left side of the assignment has type '{}', right side of the assignment has type '{}'",
+                l_data_type, r_data_type)))
+            )
+        } else {
+            Ok(response)
+        }
+    } else {
+        let line_number = parser.get_curr_line_number();
+        return Err(ParseError::SEMANTIC_ERROR(SemanticError::new(
+            parser.get_code_line(line_number, index),
+            String::from("value with type 'None' is not assignable")))
+        )
+    }
 }
