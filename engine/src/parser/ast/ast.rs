9017e6ff7f6b7397bb4363f469cd606bf18e01e6
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-07-04 16:50:31 +0530

    changes in ast

---
+++ engine/src/parser/ast/ast.rs
@@ -0,0 +1,116 @@
+use std::cell::{RefCell};
+use std::rc::Rc;
+use crate::types::{Type, CoreType};
+use crate::scope::Env;
+
+trait Node {
+    fn traverse(&self);
+}
+
+pub enum ASTNode {
+    VARIABLE_DECLARATION(VariableDeclarationNode),
+    R_ASSIGNMENT(RAssignmentNode),
+    ROUTINE_CALL(RoutineCallNode),
+    PARAMS(ParamsNode),
+    PARAM(ParamNode),
+    BEXPR(BexprNode),
+    EXPR(ExprNode),
+    ATOM(AtomNode),
+    IDENTIFIER(IdentifierNode),
+    LITERAL(LiteralNode),
+}
+
+pub struct VariableDeclarationNode(pub Rc<RefCell<CoreVariableDeclarationNode>>, pub Env);
+pub struct CoreVariableDeclarationNode {
+    pub left_side: IdentifierNode,
+    pub right_side: RAssignmentNode,
+}
+
+pub struct RAssignmentNode(pub Rc<RefCell<CoreRAssignmentNode>>, pub Env);
+pub enum CoreRAssignmentNode {
+    PARAM(ParamNode),
+}
+
+pub struct RoutineCallNode(pub Rc<RefCell<CoreRoutineCallNode>>, pub Env);
+pub struct CoreRoutineCallNode {
+    pub name: Rc<String>,
+    pub kind: RoutineKind,
+    pub params: Option<ParamsNode>,
+}
+pub enum RoutineKind {
+    FUNCTION,
+    LAMBDA,
+    CONSTRUCTOR(Rc<String>),    // struct name key to symbol table
+    METHOD(Rc<String>),         // struct name key to symbol table
+    CLASS_METHOD(Rc<String>),   // struct name key to symbol table
+}
+
+pub struct ParamsNode(pub Rc<RefCell<CoreParamsNode>>, pub Env);
+pub struct CoreParamsNode {
+    pub param: ParamNode,
+    pub remaining_params: Option<ParamsNode>,
+}
+
+pub struct ParamNode(pub Rc<RefCell<CoreParamNode>>, pub Env);
+pub enum CoreParamNode {
+    BXPR(BexprNode),
+    EXPR(ExprNode),
+    ATOM(AtomNode),
+    LITERAL(LiteralNode),
+}
+
+pub struct BexprNode(pub Rc<RefCell<CoreBexprNode>>, pub Env);
+pub struct CoreBexprNode {
+
+}
+
+pub struct ExprNode(pub Rc<RefCell<CoreExprNode>>, pub Env);
+pub struct CoreExprNode {
+
+}
+
+pub struct AtomNode(pub Rc<RefCell<CoreAtomNode>>, pub Env);
+pub struct CoreAtomNode {
+    pub kind: AtomKind,
+    pub data_type: Type,
+}
+pub enum AtomKind {
+    START(AtomStartNode),
+    INDEX_ACCESS(IndexAccessNode),
+    PROPERTRY_ACCESS(PropertryAccessNode),
+    METHOD_ACCESS(MethodAccessNode),
+}
+
+pub struct AtomStartNode(pub Rc<RefCell<CoreAtomStartNode>>, pub Env);
+pub enum CoreAtomStartNode {
+    ROUTINE_CALL(RoutineCallNode),
+    IDENTIFIER(IdentifierNode),
+}
+
+pub struct IndexAccessNode(pub Rc<RefCell<CoreIndexAccessNode>>, pub Env);
+pub struct CoreIndexAccessNode {
+    pub indexable_atom: AtomNode,
+    pub index: ParamNode,
+}
+
+pub struct PropertryAccessNode(pub Rc<RefCell<CorePropertryAccessNode>>, pub Env);
+pub struct CorePropertryAccessNode {
+    pub object: AtomNode,
+    pub propertry: Rc<String>,
+}
+
+pub struct MethodAccessNode(pub Rc<RefCell<CoreMethodAccessNode>>, pub Env);
+pub struct CoreMethodAccessNode {
+    pub object: AtomNode,
+    pub method: RoutineCallNode,
+}
+
+pub struct IdentifierNode(pub Rc<RefCell<CoreIdentifierNode>>, pub Env);
+pub struct CoreIdentifierNode {
+    pub name: Rc<String>,
+}
+
+pub struct LiteralNode(pub Rc<RefCell<CoreLiteralNode>>, pub Env);
+pub struct CoreLiteralNode {
+    pub value: Rc<String>,
+}
---
+++ engine/src/parser/ast/factory.rs
@@ -0,0 +1,117 @@
+use crate::parser::ast::ast::{VariableDeclarationNode, 
+    IdentifierNode, RAssignmentNode, ParamNode, BexprNode, ExprNode, CoreVariableDeclarationNode, CoreRAssignmentNode,
+    RoutineCallNode, RoutineKind, ParamsNode, CoreRoutineCallNode, CoreParamsNode, AtomNode, LiteralNode, AtomStartNode,
+    IndexAccessNode};
+use crate::scope::Env;
+use std::cell::RefCell;
+use std::rc::Rc;
+use crate::types::Type;
+
+use super::ast::{PropertryAccessNode, MethodAccessNode};
+
+impl VariableDeclarationNode {
+    pub fn new(env: &Env, 
+        left_side: &IdentifierNode, right_side: &RAssignmentNode) -> Self {
+        VariableDeclarationNode(Rc::new(RefCell::new(CoreVariableDeclarationNode{
+            left_side: IdentifierNode(left_side.0.clone(), Env(env.0.clone())),
+            right_side: RAssignmentNode(right_side.0.clone(), Env(env.0.clone())),
+        })), Env(env.0.clone()))
+    }
+}
+
+impl RAssignmentNode {
+    pub fn new_with_param(env: &Env, param: &ParamNode) -> Self {
+        RAssignmentNode(Rc::new(RefCell::new(
+            CoreRAssignmentNode::PARAM(ParamNode(param.0.clone(), Env(env.0.clone()))))
+        ), Env(env.0.clone()))
+    }
+}
+
+impl RoutineCallNode {
+    pub fn new(env: &Env, routine_name: &Rc<String>, kind: RoutineKind, params: Option<&ParamsNode>) -> Self {
+        RoutineCallNode(Rc::new(RefCell::new(CoreRoutineCallNode{
+            name: routine_name.clone(),
+            kind,
+            params: match params {
+                Some(params) => Some(ParamsNode(params.0.clone(), Env(env.0.clone()))),
+                None => None,
+            }
+        })), Env(env.0.clone()))
+    }
+}
+
+impl ParamsNode {
+    pub fn new(env: &Env, param: &ParamNode, remaining_params: Option<&ParamsNode>) -> Self {
+        ParamsNode(Rc::new(RefCell::new(CoreParamsNode{
+            param: ParamNode(param.0.clone(), Env(env.0.clone())),
+            remaining_params: match remaining_params {
+                Some(remaining_params) => Some(ParamsNode(remaining_params.0.clone(), Env(env.0.clone()))),
+                None => None,
+            }
+        })), Env(env.0.clone()))
+    }
+}
+
+impl ParamNode {
+    pub fn new_with_bexpr(env: &Env, bexpr: &BexprNode) -> Self {
+        todo!()
+    }
+
+    pub fn new_with_expr(env: &Env, bexpr: &ExprNode) -> Self {
+        todo!()
+    }
+
+    pub fn new_with_atom(env: &Env, bexpr: &AtomNode) -> Self {
+        todo!()
+    }
+
+    pub fn new_with_literal(env: &Env, bexpr: &LiteralNode) -> Self {
+        todo!()
+    }
+}
+
+impl BexprNode {
+    pub fn new(env: &Env) -> Self {
+        todo!()
+    }
+}
+
+impl ExprNode {
+    pub fn new(env: &Env) -> Self {
+        todo!()
+    }
+}
+
+impl AtomNode {
+    pub fn new_with_start(env: &Env, data_type: &Type, atom_start: &AtomStartNode) -> Self {
+        todo!()
+    }
+
+    pub fn new_with_index_access(env: &Env, data_type: &Type, index_access: &IndexAccessNode) -> Self {
+        todo!()
+    }
+
+    pub fn new_with_propertry_access(env: &Env, data_type: &Type, propertry_access: &PropertryAccessNode) -> Self {
+        todo!()
+    }
+
+    pub fn new_with_method_access(env: &Env, data_type: &Type, method_access: &MethodAccessNode) -> Self {
+        todo!()
+    }
+
+    pub fn get_type(&self) -> Type {
+        Type(self.0.as_ref().borrow().data_type.0.clone())
+    }
+}
+
+impl IdentifierNode {
+    pub fn new(env: &Env, name: &Rc<String>) -> Self {
+        todo!()
+    }
+}
+
+impl LiteralNode {
+    pub fn new(env: &Env, value: &Rc<String>) -> Self {
+        todo!()
+    }
+}
