4c2ede5060f516d738ac8390838ecfeabf21a116
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-07-16 08:38:16 +0530

    adding expr related changes

--- engine/src/parser/components/expression/core.rs
+++ engine/src/parser/components/expression/core.rs
@@ -1,8 +1,26 @@
-use crate::lexer::token::Token;
-
-use super::type_expression::is_type_expression_starting_with;
+use crate::{lexer::token::{Token, CoreToken}, constants::common::{INTEGER, FLOATING_POINT_NUMBER, STRING_LITERAL, 
+IDENTIFIER, ATOMIC_TYPE}, parser::parser::PackratParser};
 
 pub fn is_expression_starting_with(token: &Token) -> bool {
-    // TODO - add here other alternatives of expressions
-    is_type_expression_starting_with(token)
+    match token.core_token {
+        CoreToken::PLUS                         => true,
+        CoreToken::DASH                         => true,
+        CoreToken::NOT                          => true,
+        CoreToken::INTEGER(_)                   => true,
+        CoreToken::FLOATING_POINT_NUMBER(_)     => true,
+        CoreToken::LITERAL(_)                   => true,
+        CoreToken::IDENTIFIER(_)                => true,
+        _                                       => false,
+    }
+}
+
+pub const EXPRESSION_EXPECTED_STARTING_SYMBOLS: [&'static str; 7]
+= ["+", "-", "not", INTEGER, FLOATING_POINT_NUMBER, STRING_LITERAL, IDENTIFIER];
+
+pub fn expr(parser: &mut PackratParser) {
+    let token = &parser.curr_token();
+    if !is_expression_starting_with(token) {
+        parser.log_skipped_token_error(&EXPRESSION_EXPECTED_STARTING_SYMBOLS, &token);
+        // TODO - return missing tokens AST node
+    }
 }
--- engine/src/parser/components/stmt.rs
+++ engine/src/parser/components/stmt.rs
@@ -1,7 +1,9 @@
 use crate::ast::ast::StatementNode;
+use crate::constants::common::IDENTIFIER;
 use crate::parser::parser::{PackratParser};
 use crate::lexer::token::{Token,CoreToken};
-use crate::parser::components::expression::core::is_expression_starting_with;
+
+use super::expression::core::is_expression_starting_with;
 
 pub fn is_statement_starting_with(token: &Token) -> bool {
     match token.core_token {
@@ -13,12 +15,13 @@ pub fn is_statement_starting_with(token: &Token) -> bool {
         CoreToken::TYPE_KEYWORD         => true,
         CoreToken::INTERFACE_KEYWORD    => true,
         CoreToken::IMPL                 => true,
-        _                               => is_expression_starting_with(token)
+        CoreToken::IDENTIFIER(_)        => true,
+        _                               => is_expression_starting_with(token),
     }
 }
 
-pub const STATEMENT_EXPECTED_STARTING_SYMBOLS: [&'static str; 8] 
-= ["let", "def", "for", "while", "if", "type", "interface", "impl"];
+pub const STATEMENT_EXPECTED_STARTING_SYMBOLS: [&'static str; 9]
+= ["let", "def", "for", "while", "if", "type", "interface", "impl", IDENTIFIER];
 
 pub fn stmt(parser: &mut PackratParser) -> StatementNode {
     todo!()
