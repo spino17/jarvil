e0f8242b3189616a36d3a2e9f9fea99c53a782c1
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-07-24 13:02:14 +0530

    more ignore lines added for formatting

--- engine/src/parser/components/variable_declaration.rs
+++ engine/src/parser/components/variable_declaration.rs
@@ -5,6 +5,7 @@ pub fn variable_decl(parser: &mut PackratParser) -> VariableDeclarationNode {
     let identifier_node = parser.expect(IDENTIFIER, false);
     let equal_node = parser.expect("=", false);
     // let r_expr = parser.expr();
+    parser.ignore_newlines();
     let r_assign_node = parser.r_assign(Some(&identifier_node));
     let newline_node = parser.expects(&["\n", ENDMARKER], false);
     VariableDeclarationNode::new(&identifier_node, &r_assign_node)
--- engine/src/reader.rs
+++ engine/src/reader.rs
@@ -1,6 +1,5 @@
 use std::io;
 use std::{fs};
-use crate::code::Code;
 
 pub fn read_file(file_name: &str) -> io::Result<Vec<char>> {
     let contents = fs::read_to_string(file_name)?;
