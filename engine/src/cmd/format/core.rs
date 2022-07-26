93bc0fc65cc48660040c3f36b4dc2d2ada59f1fe
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-07-26 20:31:37 +0530

    sketching out basic flow

--- engine/src/cmd/format/core.rs
+++ engine/src/cmd/format/core.rs
@@ -4,23 +4,36 @@
 // The algorithm used for picking up optimal line breaks is heavily inspired by the writings of Bob Nystrom in the post:
 // http://journal.stuffwithstuff.com/2015/09/08/the-hardest-program-ive-ever-written/
 
-use crate::code::Code;
+use crate::ast::ast::{StatementNode, BlockNode};
 use crate::errors::ParseError;
 use crate::utils::common::build_ast;
 use crate::context;
+use std::mem;
 
 pub struct Formatter {
-
+    formatted_code_str: String,
+    indent_level: i64,
+    line_number: usize,
 }
 
 impl Formatter {
     pub fn new() -> Self {
         Formatter {
-
+            formatted_code_str: String::default(),
+            indent_level: 0,
+            line_number: 1,
         }
     }
 
-    pub fn format(code_vec: Vec<char>) -> Result<(), ParseError> {
+    pub fn format_block(&mut self, block_node: BlockNode) {
+        todo!()
+    }
+
+    pub fn format_statement(&mut self, stmt_node: StatementNode) {
+        todo!()
+    }
+
+    pub fn format(code_vec: Vec<char>) -> Result<String, ParseError> {
         let ast = build_ast(code_vec);
         match context::first_error() {
             Some(err) => {
@@ -28,11 +41,12 @@ impl Formatter {
             },
             None => {}
         }
-        let formatter = Formatter::new();
+        let mut formatter = Formatter::new();
         // TODO - use `ast` to get the formatted version of code
         // 1. walk the ast and get chunks, rules and spans tree
         // 2. use A* algorithm with number of overflowing chars and minimum splits as heuristics function
         // 3. For each value of rule, have a print method which will add appropiate line-breaks, indentation and comments
-        Ok(())
+        formatter.format_block(ast);
+        Ok(mem::take(&mut formatter.formatted_code_str))
     }
 }
