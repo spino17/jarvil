4d4900f82f8e68180b30db8ab99afac6d2b7f1d9
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-08-01 17:18:23 +0530

    making testing for reserved words generic

--- engine/src/tests/lexer/mod.rs
+++ engine/src/tests/lexer/mod.rs
@@ -1,45 +1,76 @@
+use std::vec;
+
 use crate::{constants::common::token_for_identifier, lexer::token::CoreToken};
 
-#[test]
-fn test_for() {
-    let f_vec       = vec!['f'];
-    let fo_vec      = vec!['f', 'o'];
-    let for_vec     = vec!['f', 'o', 'r'];
-    let fork_vec    = vec!['f', 'o', 'r', 'k'];
-    let forest_vec  = vec!['f', 'o', 'r', 'e', 's', 't'];
-    let alt_vec = vec![
-        f_vec, fo_vec, for_vec, fork_vec, forest_vec,
-    ];
-    for (index, word) in alt_vec.iter().enumerate() {
-        let word_iter: std::slice::Iter<char> = word.iter();
-        let token_type = if index == 2 {
-            CoreToken::FOR
+fn str_to_vec(word: &str) -> Vec<char> {
+    word.chars().collect()
+}
+
+fn assert_token_for_identifier(alt_vec: Vec<&str>, reserved_word: &str, reserved_word_token_type: CoreToken) {
+    for word in alt_vec {
+        let word_vec = str_to_vec(word);
+        let word_iter: std::slice::Iter<char> = word_vec.iter();
+        let token_type = if word == reserved_word {
+            reserved_word_token_type.clone()
         } else {
             CoreToken::IDENTIFIER
         };
-        assert_eq!(token_for_identifier(word_iter), token_type, "word passed: {}", word.iter().collect::<String>());
+        assert_eq!(token_for_identifier(word_iter), token_type, "word passed: {}", word);
     }
 }
 
 #[test]
-fn test_while() {
-    let w_vec           = vec!['w'];
-    let wh_vec          = vec!['w', 'h'];
-    let whi_vec         = vec!['w', 'h', 'i'];
-    let whil_vec        = vec!['w', 'h', 'i', 'l'];
-    let while_vec       = vec!['w', 'h', 'i', 'l', 'e'];
-    let whiles_vec      = vec!['w', 'h', 'i', 'l', 'e', 's'];
-    let whilesies_vec   = vec!['w', 'h', 'i', 'l', 'e', 's', 'i', 'e', 's'];
+fn test_general() {
     let alt_vec = vec![
-        w_vec, wh_vec, whi_vec, whil_vec, while_vec, whiles_vec, whilesies_vec
+        "zebra", "dungeon_master", "x", "y"
     ];
-    for (index, word) in alt_vec.iter().enumerate() {
-        let word_iter: std::slice::Iter<char> = word.iter();
-        let token_type = if index == 4 {
-            CoreToken::WHILE
-        } else {
-            CoreToken::IDENTIFIER
-        };
-        assert_eq!(token_for_identifier(word_iter), token_type, "word passed: {}", word.iter().collect::<String>());
-    }
+    assert_token_for_identifier(alt_vec, "x", CoreToken::IDENTIFIER)
+}
+
+#[test]
+fn test_for() {
+    let alt_vec = vec![
+        "f", "fo", "for", "fog", "fork", "forest",
+    ];
+    assert_token_for_identifier(alt_vec, "for", CoreToken::FOR)
+}
+
+#[test]
+fn test_while() {
+    let alt_vec = vec![
+        "w", "was", "whi", "while", "whilt", "whales"
+    ];
+    assert_token_for_identifier(alt_vec, "while", CoreToken::WHILE)
+}
+
+#[test]
+fn test_continue() {
+    let alt_vec = vec![
+        "c", "country", "continue", "continus", "continuation"
+    ];
+    assert_token_for_identifier(alt_vec, "continue", CoreToken::CONTINUE)
+}
+
+#[test]
+fn test_break() {
+    let alt_vec = vec![
+        "b", "brag", "break", "boat", "brake", "breaking"
+    ];
+    assert_token_for_identifier(alt_vec, "break", CoreToken::BREAK)
+}
+
+#[test]
+fn test_if() {
+    let alt_vec = vec![
+        "i", "if", "is", "ifconfig"
+    ];
+    assert_token_for_identifier(alt_vec, "if", CoreToken::IF)
+}
+
+#[test]
+fn test_else() {
+    let alt_vec = vec![
+        "e", "el", "else", "elas", "elsex", "elite"
+    ];
+    assert_token_for_identifier(alt_vec, "else", CoreToken::ELSE)
 }
