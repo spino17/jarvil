113d8dc06b5655f14512cbd812f8c610f52e3194
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-08-30 15:23:30 +0530

    some basic code for type-checker

--- engine/src/parser/type_checker.rs
+++ engine/src/parser/type_checker.rs
@@ -1,21 +1,40 @@
-use crate::{scope::core::Namespace, code::Code, ast::{ast::{BlockNode, ASTNode}, walk::Visitor}};
+use crate::{scope::core::Namespace, code::Code, ast::{ast::{BlockNode, ASTNode, StatementNode}, walk::Visitor}, error::core::JarvilError};
 
 pub struct TypeChecker {
     namespace: Namespace,
     code: Code,
-    ast: BlockNode,
+    errors: Vec<JarvilError>,
 }
 impl TypeChecker {
-    pub fn new(code: &Code, ast: &BlockNode) -> Self {
+    pub fn new(code: &Code, scope: &Namespace) -> Self {
         TypeChecker{
-            namespace: ast.scope().expect("scope should be set to the `BlockNode` during resolver phase"),
+            namespace: scope.clone(),
             code: code.clone(),
-            ast: ast.clone(),
+            errors: vec![],
         }
+    }
+
+    pub fn check_ast(&mut self, ast: &BlockNode) -> Vec<JarvilError> {
+        let core_block = ast.0.as_ref().borrow();
+        for stmt in &core_block.stmts {
+            self.walk_stmt_indent_wrapper(stmt);
+        }
+        std::mem::take(&mut self.errors)
+    }
+
+    pub fn check_stmt(&mut self, stmt: &StatementNode) {
+        // TODO - type-check all the statements here
+        todo!()
     }
 }
 impl Visitor for TypeChecker {
     fn visit(&mut self, node: &ASTNode) -> Option<()> {
-        todo!()
+        match node {
+            ASTNode::STATEMENT(stmt) => {
+                self.check_stmt(stmt);
+                return None
+            },
+            _ => Some(())
+        }
     }
 }
