1f92c1286632ddd548844322066f35a16b3e6094
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-09-11 12:55:51 +0530

    adding constructors for all errors

--- engine/src/error/diagnostics.rs
+++ engine/src/error/diagnostics.rs
@@ -1,4 +1,4 @@
-use super::helper::{IdentifierKind, PropertyKind, range_to_span};
+use super::helper::{range_to_span, IdentifierKind, PropertyKind};
 use crate::{lexer::token::Token, parser::helper::format_symbol, types::core::Type};
 use miette::{Diagnostic, LabeledSpan, Report, SourceSpan};
 use std::fmt::{self, Display};
@@ -51,7 +51,9 @@ impl Diagnostics {
             Diagnostics::ExpressionNotCallable(diagnostic) => Report::new(diagnostic.clone()),
             Diagnostics::ExpressionIndexingNotValid(diagnostic) => Report::new(diagnostic.clone()),
             Diagnostics::UnaryOperatorInvalidUse(diagnostic) => Report::new(diagnostic.clone()),
-            Diagnostics::BinaryOperatorInvalidOperands(diagnostic) => Report::new(diagnostic.clone()),
+            Diagnostics::BinaryOperatorInvalidOperands(diagnostic) => {
+                Report::new(diagnostic.clone())
+            }
             Diagnostics::MismatchedTypesOnLeftRight(diagnostic) => Report::new(diagnostic.clone()),
             Diagnostics::NoReturnStatementInFunction(diagnostic) => Report::new(diagnostic.clone()),
             Diagnostics::InvalidReturnStatement(diagnostic) => Report::new(diagnostic.clone()),
@@ -78,7 +80,7 @@ impl InvalidCharError {
     pub fn new(invalid_char: char, range: TextRange) -> Self {
         InvalidCharError {
             invalid_char,
-            span: range_to_span(range).into()
+            span: range_to_span(range).into(),
         }
     }
 }
@@ -97,7 +99,7 @@ impl NoClosingSymbolError {
     pub fn new(expected_symbol: char, range: TextRange) -> Self {
         NoClosingSymbolError {
             expected_symbol,
-            unclosed_span: range_to_span(range).into()
+            unclosed_span: range_to_span(range).into(),
         }
     }
 }
@@ -153,7 +155,11 @@ impl Diagnostic for MissingTokenError {
             ));
             err_str
         };
-        let span_vec = vec![LabeledSpan::new(Some(err_message), self.start_index, self.len)];
+        let span_vec = vec![LabeledSpan::new(
+            Some(err_message),
+            self.start_index,
+            self.len,
+        )];
         return Some(Box::new(span_vec.into_iter()));
     }
 }
@@ -201,6 +207,14 @@ pub struct InvalidLValueError {
     #[help]
     pub help: Option<String>, // any value derived from a function call is not assignable
 }
+impl InvalidLValueError {
+    pub fn new(range: TextRange) -> Self {
+        InvalidLValueError {
+            span: range_to_span(range).into(),
+            help: Some("any value derived from a function call is not assignable".to_string()),
+        }
+    }
+}
 
 #[derive(Diagnostic, Debug, Error, Clone)]
 #[error("{} is redeclared", self.identifier_kind)]
@@ -209,12 +223,36 @@ pub struct IdentifierAlreadyDeclaredError {
     pub identifier_kind: IdentifierKind,
     pub name: String,
     #[label("previous declaration of `{}` is here", self.name)]
-    pub previous_def_span: SourceSpan,
+    pub previous_decl_span: SourceSpan,
     #[label("`{}` is redeclared here", self.name)]
-    pub redef_span: SourceSpan,
+    pub redecl_span: SourceSpan,
     #[help]
-    pub help: Option<String>, // variables and functions are not allowed to be redeclared inside the same block
-                              // whereas types are not allowed to be redeclared inside the complete scope
+    pub help: Option<String>,
+}
+impl IdentifierAlreadyDeclaredError {
+    pub fn new(
+        identifier_kind: IdentifierKind,
+        name: String,
+        previous_decl_range: TextRange,
+        redecl_range: TextRange,
+    ) -> Self {
+        let help_str = match identifier_kind {
+            IdentifierKind::VARIABLE | IdentifierKind::FUNCTION => {
+                "variables and functions are not allowed to be redeclared inside the same block"
+                    .to_string()
+            }
+            IdentifierKind::TYPE => {
+                "types are not allowed to be redeclared inside the complete scope".to_string()
+            }
+        };
+        IdentifierAlreadyDeclaredError {
+            identifier_kind,
+            name,
+            previous_decl_span: range_to_span(previous_decl_range).into(),
+            redecl_span: range_to_span(redecl_range).into(),
+            help: Some(help_str),
+        }
+    }
 }
 
 #[derive(Diagnostic, Debug, Error, Clone)]
@@ -226,7 +264,17 @@ pub struct IdentifierNotDeclaredError {
     #[label("`{}` not found in the scope", self.name)]
     pub span: SourceSpan,
     #[help]
-    help: Option<String>, // identifiers are declared into one of the three namespaces: variables, functions and types
+    help: Option<String>,
+}
+impl IdentifierNotDeclaredError {
+    pub fn new(identifier_kind: IdentifierKind, name: String, range: TextRange) -> Self {
+        IdentifierNotDeclaredError {
+            identifier_kind,
+            name,
+            span: range_to_span(range).into(),
+            help: Some("identifiers are declared in one of the three namespaces: variables, functions and types".to_string())
+        }
+    }
 }
 
 #[derive(Diagnostic, Debug, Error, Clone)]
@@ -236,6 +284,14 @@ pub struct MoreParamsCountError {
     expected_params_count: usize,
     #[label("expected `{}` arguments, got more than that", self.expected_params_count)]
     pub span: SourceSpan,
+}
+impl MoreParamsCountError {
+    pub fn new(expected_params_count: usize, range: TextRange) -> Self {
+        MoreParamsCountError {
+            expected_params_count,
+            span: range_to_span(range).into(),
+        }
+    }
 }
 
 #[derive(Diagnostic, Debug, Error, Clone)]
@@ -247,11 +303,29 @@ pub struct LessParamsCountError {
     #[label("expected {} arguments, got {}", self.expected_params_count, self.received_params_count)]
     pub span: SourceSpan,
 }
+impl LessParamsCountError {
+    pub fn new(
+        expected_params_count: usize,
+        received_params_count: usize,
+        range: TextRange,
+    ) -> Self {
+        LessParamsCountError {
+            expected_params_count,
+            received_params_count,
+            span: range_to_span(range).into(),
+        }
+    }
+}
 
 #[derive(Debug, Error, Clone)]
 #[error("mismatched types")]
 pub struct MismatchedParamTypeError {
     params_vec: Vec<(String, String, usize, TextRange)>, // (expected_type, received_type, index_of_param, span)
+}
+impl MismatchedParamTypeError {
+    pub fn new(params_vec: Vec<(String, String, usize, TextRange)>) -> Self {
+        MismatchedParamTypeError { params_vec }
+    }
 }
 impl Diagnostic for MismatchedParamTypeError {
     fn labels(&self) -> Option<Box<dyn Iterator<Item = miette::LabeledSpan> + '_>> {
@@ -283,7 +357,17 @@ pub struct IdentifierNotCallableError {
     #[label("variable `{}` with type `{}` is not callable", self.name, self.ty)]
     pub span: SourceSpan,
     #[help]
-    pub help: Option<String>, // only variables with lambda types are callable
+    pub help: Option<String>,
+}
+impl IdentifierNotCallableError {
+    pub fn new(name: String, ty: Type, range: TextRange) -> Self {
+        IdentifierNotCallableError {
+            name,
+            ty: ty.to_string(),
+            span: range_to_span(range).into(),
+            help: Some("only variables with lambda types are callable".to_string()),
+        }
+    }
 }
 
 #[derive(Diagnostic, Debug, Error, Clone)]
@@ -298,6 +382,23 @@ pub struct PropertyDoesNotExistError {
     #[label("expression has type `{}`", self.ty)]
     pub expr_span: SourceSpan,
 }
+impl PropertyDoesNotExistError {
+    pub fn new(
+        property_kind: PropertyKind,
+        property_name: String,
+        ty: Type,
+        property_range: TextRange,
+        expr_range: TextRange,
+    ) -> Self {
+        PropertyDoesNotExistError {
+            property_kind,
+            property_name,
+            ty: ty.to_string(),
+            property_span: range_to_span(property_range).into(),
+            expr_span: range_to_span(expr_range).into(),
+        }
+    }
+}
 
 #[derive(Diagnostic, Debug, Error, Clone)]
 #[error("property not supported")]
@@ -307,6 +408,14 @@ pub struct PropertyNotSupportedError {
     #[label("{} are only supported for `struct` types", self.property_name)]
     pub span: SourceSpan,
 }
+impl PropertyNotSupportedError {
+    pub fn new(property_name: String, range: TextRange) -> Self {
+        PropertyNotSupportedError {
+            property_name,
+            span: range_to_span(range).into(),
+        }
+    }
+}
 
 #[derive(Diagnostic, Debug, Error, Clone)]
 #[error("calling an uncallable")]
@@ -314,6 +423,13 @@ pub struct PropertyNotSupportedError {
 pub struct ExpressionNotCallableError {
     #[label("expression is not callable")]
     pub span: SourceSpan,
+}
+impl ExpressionNotCallableError {
+    pub fn new(range: TextRange) -> Self {
+        ExpressionNotCallableError {
+            span: range_to_span(range).into(),
+        }
+    }
 }
 
 #[derive(Diagnostic, Debug, Error, Clone)]
@@ -327,6 +443,21 @@ pub struct ExpressionIndexingNotValidError {
     #[label("expression with type `{}` is not indexable with value of type `{}`", self.expr_type, self.index_type)]
     pub index_span: SourceSpan,
 }
+impl ExpressionIndexingNotValidError {
+    pub fn new(
+        expr_ty: Type,
+        index_ty: Type,
+        expr_range: TextRange,
+        index_range: TextRange,
+    ) -> Self {
+        ExpressionIndexingNotValidError {
+            expr_type: expr_ty.to_string(),
+            index_type: index_ty.to_string(),
+            expr_span: range_to_span(expr_range).into(),
+            index_span: range_to_span(index_range).into(),
+        }
+    }
+}
 
 #[derive(Diagnostic, Debug, Error, Clone)]
 #[error("invalid unary operand")]
@@ -334,12 +465,35 @@ pub struct ExpressionIndexingNotValidError {
 pub struct UnaryOperatorInvalidUseError {
     pub ty: String,
     pub valid_operand_type: String,
+    pub operator: String,
     #[label("operand has type `{}`", self.ty)]
     pub operand_span: SourceSpan,
     #[label("operator cannot be applied on operand")]
     pub operator_span: SourceSpan,
     #[help]
     pub help: Option<String>, // unary operator {} is valid only for {} operands
+}
+impl UnaryOperatorInvalidUseError {
+    pub fn new(
+        ty: Type,
+        valid_operand_type: String,
+        operator: String,
+        operand_range: TextRange,
+        operator_range: TextRange,
+    ) -> Self {
+        let help_str = format!(
+            "unary operator {} is valid only for {} operands",
+            operator, valid_operand_type
+        );
+        UnaryOperatorInvalidUseError {
+            ty: ty.to_string(),
+            valid_operand_type,
+            operator,
+            operand_span: range_to_span(operand_range).into(),
+            operator_span: range_to_span(operator_range).into(),
+            help: Some(help_str),
+        }
+    }
 }
 
 #[derive(Diagnostic, Debug, Error, Clone)]
@@ -357,6 +511,25 @@ pub struct BinaryOperatorInvalidOperandsError {
     #[help]
     pub help: Option<String>, // binary operator {} is valid only for {} operands
 }
+impl BinaryOperatorInvalidOperandsError {
+    pub fn new(
+        left_type: Type,
+        right_type: Type,
+        left_range: TextRange,
+        right_range: TextRange,
+        operator_range: TextRange,
+    ) -> Self {
+        // TODO - construct dynamic help message
+        BinaryOperatorInvalidOperandsError {
+            left_type: left_type.to_string(),
+            right_type: right_type.to_string(),
+            left_expr_span: range_to_span(left_range).into(),
+            right_expr_span: range_to_span(right_range).into(),
+            operator_span: range_to_span(operator_range).into(),
+            help: None,
+        }
+    }
+}
 
 #[derive(Diagnostic, Debug, Error, Clone)]
 #[error("mismatched types")]
@@ -371,6 +544,22 @@ pub struct MismatchedTypesOnLeftRightError {
     #[help]
     pub help: Option<String>, // types on both sides should be same
 }
+impl MismatchedTypesOnLeftRightError {
+    pub fn new(
+        left_type: Type,
+        right_type: Type,
+        left_range: TextRange,
+        right_range: TextRange,
+    ) -> Self {
+        MismatchedTypesOnLeftRightError {
+            left_type: left_type.to_string(),
+            right_type: right_type.to_string(),
+            left_span: range_to_span(left_range).into(),
+            right_span: range_to_span(right_range).into(),
+            help: Some("types on both sides should be same".to_string()),
+        }
+    }
+}
 
 #[derive(Diagnostic, Debug, Error, Clone)]
 #[error("no return statement found")]
@@ -379,7 +568,15 @@ pub struct NoReturnStatementInFunctionError {
     #[label("function body has no `return` statement")]
     pub span: SourceSpan,
     #[help]
-    pub help: Option<String>, // function with a return value should have atleast one `return` statement inside the top-level block
+    pub help: Option<String>,
+}
+impl NoReturnStatementInFunctionError {
+    pub fn new(range: TextRange) -> Self {
+        NoReturnStatementInFunctionError {
+            span: range_to_span(range).into(),
+            help: Some("function with a return value should have atleast one `return` statement inside the top-level block".to_string())
+        }
+    }
 }
 
 #[derive(Diagnostic, Debug, Error, Clone)]
@@ -389,7 +586,15 @@ pub struct InvalidReturnStatementError {
     #[label("invalid `return` statement")]
     pub span: SourceSpan,
     #[help]
-    pub help: Option<String>, // `return` statement should be used inside function body
+    pub help: Option<String>,
+}
+impl InvalidReturnStatementError {
+    pub fn new(range: TextRange) -> Self {
+        InvalidReturnStatementError {
+            span: range_to_span(range).into(),
+            help: Some("`return` statement should be used inside function body".to_string()),
+        }
+    }
 }
 
 #[derive(Diagnostic, Debug, Error, Clone)]
@@ -400,4 +605,13 @@ pub struct MismatchedReturnTypeError {
     pub received_type: String,
     #[label("expected return value type `{}`, got `{}`", self.expected_type, self.received_type)]
     pub span: SourceSpan,
+}
+impl MismatchedReturnTypeError {
+    pub fn new(expected_type: Type, received_type: Type, range: TextRange) -> Self {
+        MismatchedReturnTypeError {
+            expected_type: expected_type.to_string(),
+            received_type: received_type.to_string(),
+            span: range_to_span(range).into(),
+        }
+    }
 }
