73696291a11375d0e51df56fe5ef5a75f2d6f27a
Bhavya Bhatt <bhavyabhatt@Bhavyas-MacBook-Pro.local>
2022-09-16 23:26:42 +0530

    adding code for grow

--- engine/src/backend/object/dict.rs
+++ engine/src/backend/object/dict.rs
@@ -7,18 +7,41 @@ const INIT_CAPACITY: usize = 8;
 const LOAD_FACTOR: f64 = 2.0 / 3.0;
 // const SEED: byte = ...
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct OkEntry {
     hash: usize,
     key: Data,
     value: Data,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Entry {
     OK(OkEntry), // active entries
     TOMBSTONE,   // soft-deleted entries
     NULL,        // empty slots
+}
+
+impl Entry {
+    fn is_ok(&self) -> bool {
+        match self {
+            Entry::OK(_) => true,
+            _ => false,
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        match self {
+            Entry::NULL => true,
+            _ => false,
+        }
+    }
+
+    fn is_tombstone(&self) -> bool {
+        match self {
+            Entry::TOMBSTONE => true,
+            _ => false,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -49,7 +72,7 @@ impl CoreDictObject {
     }
 
     fn allocate(len: usize) -> NonNull<Entry> {
-        let layout = CoreDictObject::layout(INIT_CAPACITY);
+        let layout = CoreDictObject::layout(len);
         assert!(layout.size() <= isize::MAX as usize, "allocation too large");
         let ptr = unsafe { alloc::alloc(layout) };
         let new_ptr = match NonNull::new(ptr as *mut Entry) {
@@ -59,23 +82,53 @@ impl CoreDictObject {
         new_ptr
     }
 
-    fn grow() {
-        // as soon as count goes up by load_factor * capacity we call grow(). We check this on insert.
-        // grow basically takes each entry in the dictionary, calculate it's new hash based on new capacity
-        // and insert into the new dictionary array.
-        // finally override the entry pointer into the object to new array location.
-        todo!()
+    fn grow(&mut self) {
+        let new_cap = 2 * self.cap;
+        let new_ptr = CoreDictObject::allocate(new_cap);
+        unsafe {
+            for i in 0..new_cap {
+                ptr::write(new_ptr.as_ptr().add(i), Entry::NULL);
+            }
+            for i in 0..self.cap {
+                let (key, value) = match &*self.ptr.as_ptr().add(i) {
+                    Entry::OK(ok_entry) => (&ok_entry.key, &ok_entry.value),
+                    _ => continue,
+                };
+                let (entry_index, hash) = self.find_entry(new_ptr.clone(), new_cap, key);
+                *new_ptr.as_ptr().add(entry_index) = Entry::OK(OkEntry {
+                    hash,
+                    key: key.clone(),
+                    value: value.clone(),
+                });
+            }
+        }
+        self.cap = new_cap;
+        self.ptr = new_ptr;
     }
 
-    fn find_entry(&self, ptr: NonNull<Entry>, cap: usize, key: Data) -> usize {
+    fn find_entry(&self, ptr: NonNull<Entry>, cap: usize, key: &Data) -> (usize, usize) {
+        // (entry_index, hash)
         // finds entry with key `key` for capacity `cap` in the array pointed by `ptr`
         // either finds the index where it matches the key or if slot is empty
         todo!()
     }
 
-    fn insert(&self, key: Data, value: Data) -> Data {
-        // returns the old value if key already exists
-        todo!()
+    fn insert(&mut self, key: Data, value: Data) -> Option<Data> {
+        if self.count as f64 > (self.cap as f64 * LOAD_FACTOR) {
+            self.grow();
+        }
+        let (entry_index, hash) = self.find_entry(self.ptr.clone(), self.cap, &key);
+        unsafe {
+            let old_value = match &*self.ptr.as_ptr().add(entry_index) {
+                Entry::OK(ok_entry) => Some(ok_entry.value.clone()),
+                _ => {
+                    self.count = self.count + 1;
+                    None
+                }
+            };
+            *self.ptr.as_ptr().add(entry_index) = Entry::OK(OkEntry { hash, key, value });
+            return old_value;
+        }
     }
 
     fn delete(&self, key: Data) -> Data {
@@ -85,6 +138,15 @@ impl CoreDictObject {
 
     fn has_key(&self, key: Data) -> bool {
         todo!()
+    }
+}
+
+impl Drop for CoreDictObject {
+    fn drop(&mut self) {
+        let layout = CoreDictObject::layout(self.cap);
+        unsafe {
+            alloc::dealloc(self.ptr.as_ptr() as *mut u8, layout);
+        }
     }
 }
 
