65a29fbc597439ea9e93a22acef880d52e7689e4
spino17 <bhavyabhatt17@gmail.com>
2023-05-24 15:33:03 +0530

    adding check cmd in version also

--- src/tools/anyon/version.rs
+++ src/tools/anyon/version.rs
@@ -1,5 +1,5 @@
 use super::{core::AbstractCommand, error::AnyonError};
-use crate::error::diagnostics::Diagnostics;
+use crate::error::{diagnostics::Diagnostics, constants::TOO_MANY_COMMAND_LINE_ARGUMENTS_PASSED_ERROR_MSG};
 
 #[derive(Debug)]
 pub struct VersionDriver {
@@ -10,19 +10,21 @@ impl VersionDriver {
     pub fn new(command_line_args: Vec<String>) -> Self {
         VersionDriver { command_line_args }
     }
-
-    pub fn get_version(&self) -> String {
-        todo!()
-    }
 }
 
 impl AbstractCommand for VersionDriver {
     fn check_cmd(&mut self) -> Result<(), AnyonError> {
-        todo!()
+        let len = self.command_line_args.len();
+        if len > 2 {
+            return Err(AnyonError::new_with_vanilla(TOO_MANY_COMMAND_LINE_ARGUMENTS_PASSED_ERROR_MSG.to_string()))
+        }
+        Ok(())
     }
 
     fn execute_cmd(&self) -> Result<(), AnyonError> {
-        todo!()
+        // TODO - change this to take the value of version from `.toml` file
+        println!("jarvil 0.1.0");
+        Ok(())
     }
 
     fn help_str(&self) -> String {
