9cb039d940d585a57acb2ab4732a5c783c674632
spino17 <bhavyabhatt17@gmail.com>
2023-05-26 10:19:03 +0530

    removing rc from JarvilCode

--- src/code.rs
+++ src/code.rs
@@ -1,22 +1,21 @@
-use std::rc::Rc;
 use text_size::TextRange;
 
 pub struct JarvilCode {
-    code_vec: Rc<Vec<char>>,
-    code_lines: Option<Rc<Vec<usize>>>,
+    code_vec: Vec<char>,
+    code_lines: Option<Vec<usize>>
 }
 
 impl JarvilCode {
     pub fn new(code_vec: Vec<char>) -> Self {
         JarvilCode {
-            code_vec: Rc::new(code_vec),
+            code_vec: code_vec,
             code_lines: None,
         }
     }
 
-    pub fn extract_code_lines(&self) -> Rc<Vec<usize>> {
+    pub fn extract_code_lines(&self) -> &Vec<usize> {
         match &self.code_lines {
-            Some(code_lines) => return code_lines.clone(),
+            Some(code_lines) => return code_lines,
             None => unreachable!(
                 "this method should always be called once code_lines has been set by the lexer"
             ),
@@ -37,7 +36,7 @@ impl JarvilCode {
     }
 
     pub fn set_code_lines(&mut self, code_lines: Vec<usize>) {
-        self.code_lines = Some(Rc::new(code_lines));
+        self.code_lines = Some(code_lines);
     }
 
     pub fn token_value(&self, start_index: usize, end_index: Option<usize>) -> String {
