b1efb216e4afc16072087877af509d2626bfa11c
spino17 <bhavyabhatt17@gmail.com>
2024-01-31 15:36:23 +0530

    adding more robust flow for macros

--- compiler/jarvil-macros/src/token.rs
+++ compiler/jarvil-macros/src/token.rs
@@ -4,13 +4,54 @@ use proc_macro::*;
 use quote::quote;
 
 pub fn impl_tokenify_macro(ast: &syn::DeriveInput) -> TokenStream {
-    let enum_name = &ast.ident;
-    if !enum_name.to_string().eq("CoreToken") {
+    if !ast.ident.to_string().eq("CoreToken") {
         panic!("tokenify macro should only be used for `crate::lexer::token::CoreToken` enum")
     }
-    let enum_data = match &ast.data {
-        syn::Data::Enum(enum_data) => enum_data,
-        _ => panic!("tokenify macro should only be used for `crate::lexer::token::CoreToken` enum"),
+    let syn::Data::Enum(enum_data) = &ast.data else {
+        unreachable!()
+    };
+    let variants = &enum_data.variants;
+    let token_eq_funcs = variants.iter().map(|variant| {
+        let variant_name = &variant.ident;
+        quote! {
+            fn #variant_name(&self) -> bool {
+                match self {
+                    CoreToken::#variant_name => true,
+                    _ => false,
+                }
+            }
+        }
+    });
+    let matching_arms_for_is_eq = variants.iter().map(|variant| {
+        let variant_name = &variant.ident;
+        quote! {
+            #variant_name => self.#variant_name(),
+        }
+    });
+    let is_eq_func = quote! {
+        pub fn is_eq(&self, symbol: &str) -> bool {
+            match symbol {
+                #(#matching_arms_for_is_eq)*
+                "\n" => self.NEWLINE(),
+                _ => unreachable!("invalid symbol `{}` found", symbol),
+            }
+        }
+    };
+
+    let matching_arms_for_to_string = variants.iter().map(|variant| {
+        let variant_name = &variant.ident;
+        quote! {
+            CoreToken::#variant_name => #variant_name,
+        }
+    });
+    let to_string_func = quote! {
+        pub fn to_string(&self) -> &'static str {
+            let symbol_str = match self {
+                #(#matching_arms_for_to_string)*
+                CoreToken::LEXICAL_ERROR(_) => LEXICAL_ERROR,
+            };
+            symbol_str
+        }
     };
     let variant_iter = &mut enum_data.variants.iter();
     let mut args_str = "".to_string();
@@ -30,7 +71,7 @@ pub fn impl_tokenify_macro(ast: &syn::DeriveInput) -> TokenStream {
     args_str.push_str(", LEXICAL_ERROR");
     let symbols_is_eq = get_macro_expr_stmt("impl_symbols_is_eq", &args_str);
     let gen = quote! {
-        impl #enum_name {
+        impl CoreToken {
             #symbols_check_macro_stmt;
             fn LEXICAL_ERROR(&self) -> bool {
                 match self {
